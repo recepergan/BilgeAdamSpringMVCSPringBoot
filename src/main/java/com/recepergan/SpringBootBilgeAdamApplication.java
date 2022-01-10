018107a4e95b1f78d4f1a3216fdceb58fd06357b
recepergan <rcpergan@gmail.com>
2022-01-10 09:52:50 +0300

    Initial commit

---
+++ src/main/java/com/recepergan/SpringBootBilgeAdamApplication.java
@@ -0,0 +1,13 @@
+package com.recepergan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBilgeAdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBilgeAdamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recepergan/SpringBootBilgeAdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recepergan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBilgeAdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
